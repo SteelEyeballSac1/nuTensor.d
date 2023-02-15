58cf40cd7e494547bb21898e3c57763b5edfa8d6
SteelEyeballSac1 <76492954+SteelEyeballSac1@users.noreply.github.com>
2023-02-15 13:01:29 -0800

    Update make-firefox.sh

--- tools/make-firefox.sh
+++ tools/make-firefox.sh
@@ -2,11 +2,11 @@
 #
 # This script assumes a linux environment
 
-echo "*** nuTensor.firefox: Creating web store package"
-echo "*** nuTensor.firefox: Copying files"
+echo "*** nuTensor.d.firefox: Creating web store package"
+echo "*** nuTensor.d.firefox: Copying files"
 
 BLDIR=dist/build
-DES="$BLDIR"/nuTensor.firefox
+DES="$BLDIR"/nuTensor.d.firefox
 rm -rf $DES
 mkdir -p $DES
 
@@ -20,20 +20,20 @@ cp    LICENSE.txt                           $DES/
 cp    platform/firefox/*.js                 $DES/js/
 cp    platform/firefox/manifest.json        $DES/
 
-echo "*** nuTensor.firefox: Generating meta..."
+echo "*** nuTensor.d.firefox: Generating meta..."
 python3 tools/make-firefox-meta.py          $DES/
 
 if [ "$1" = all ]; then
-    echo "*** nuTensor.firefox: Creating package..."
+    echo "*** nuTensor.d.firefox: Creating package..."
     pushd $DES > /dev/null
     zip ../$(basename $DES).xpi -qr *
     popd > /dev/null
 elif [ -n "$1" ]; then
-    echo "*** nuTensor.firefox: Creating versioned package..."
+    echo "*** nuTensor.d.firefox: Creating versioned package..."
     pushd $DES > /dev/null
     zip ../$(basename $DES).xpi -qr *
     popd > /dev/null
-    mv "$BLDIR"/nuTensor.firefox.xpi "$BLDIR"/nuTensor"$1".firefox.xpi
+    mv "$BLDIR"/nuTensor.d.firefox.xpi "$BLDIR"/nuTensor.d"$1".firefox.xpi
 fi
 
-echo "*** nuTensor.firefox: Package done."
+echo "*** nuTensor.d.firefox: Package done."
