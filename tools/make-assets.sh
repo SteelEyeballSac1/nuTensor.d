d1df27875bcff3a5666400288e0ea0c289eed183
SteelEyeballSac1 <76492954+SteelEyeballSac1@users.noreply.github.com>
2023-04-11 09:59:52 -0700

    Update make-assets.sh

--- tools/make-assets.sh
+++ tools/make-assets.sh
@@ -13,19 +13,13 @@ cp ./assets/assets.json $DES/
 
 if [ -n "${TRAVIS_TAG}" ]; then
   pushd .. > /dev/null
-  git clone --depth 1 https://github.com/geekprojects/nuAssets.git
+  git clone --depth 1 https://github.com/SteelEyeballSac1/nuAssets.d.git
   popd > /dev/null
 fi
 
 mkdir $DES/thirdparties
-cp -R ../nuAssets/thirdparties/hosts-file.net             $DES/thirdparties/
-cp -R ../nuAssets/thirdparties/mirror1.malwaredomains.com $DES/thirdparties/
-cp -R ../nuAssets/thirdparties/pgl.yoyo.org               $DES/thirdparties/
-cp -R ../nuAssets/thirdparties/publicsuffix.org           $DES/thirdparties/
-cp -R ../nuAssets/thirdparties/someonewhocares.org        $DES/thirdparties/
-cp -R ../nuAssets/thirdparties/winhelp2002.mvps.org       $DES/thirdparties/
-cp -R ../nuAssets/thirdparties/www.malwaredomainlist.com  $DES/thirdparties/
-mkdir $DES/umatrix
-cp -R ../nuAssets/recipes/*                               $DES/umatrix/
+cp -R ../nuAssets.d/thirdparties/*             $DES/thirdparties/
+mkdir $DES/nuTensor.d
+cp -R ../nuAssets.d/recipes/*                  $DES/umatrix/
 
 echo "done."
